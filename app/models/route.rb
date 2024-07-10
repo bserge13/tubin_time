fbc1e5c0d0c9920f74093f5c65f034c569d090f0
Kam Kennedy <kameronk013@gmail.com>
2024-07-10 16:41:48 -0600

    create model method for status

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,17 @@
 class Route < ApplicationRecord
   validates :title, :address, :city, :state, :flow_max, :flow_min, presence: true
+
+  def status
+    if current_flow > flow_max
+      "High"
+    elsif current_flow < flow_min
+      "Low"
+    else
+      "Safe to Tube"
+    end
+  end
+
+  def current_flow
+    RouteFacade.get_current_flow(address)
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.before(:suite) do
+    Rails.application.load_seed # Loading the seeds from db/seeds.rb
+  end
 end
