421f49e34c96cefe4385b325186c24c6fe06fcf3
Blake Sergesketter <blakesergesketter@gmail.com>
2024-07-12 11:05:06 -0500

    Setup db on local machine. Model testing for Route model, and instance-methods current_flow and status. All testing passes- safe to send it, breh.

---
+++ spec/models/route.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Route, type: :model do 
+    describe 'validations' do  
+        it { should validate_presence_of :title }
+        it { should validate_presence_of :address }
+        it { should validate_presence_of :city }
+        it { should validate_presence_of :state }
+        it { should validate_presence_of :flow_max }
+        it { should validate_presence_of :flow_min }
+    end
+
+    describe '#instance_method' do 
+        it '#current_flow', :vcr do
+            expect(Route.last.current_flow).to be_a Float
+        end
+
+        it '#status', :vcr do
+            expect(Route.last.status).to be_a String
+        end
+    end
+end
+
